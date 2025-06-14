0368c53ca021005ac7b1a4975471a49d6f65398b
Matthew McPherrin <git@mcpherrin.ca>
2025-06-14 12:37:05 -0400

    Graph the data

--- munge.py
+++ munge.py
@@ -1,6 +1,7 @@
 import collections
 import csv
 import json
+import pygal
 
 def read_telemetry():
     raw = open("cert.validation_success_by_ca.json").read()
@@ -29,6 +30,7 @@ def read_bin_names():
 bin_names = read_bin_names()
 
 CAs = collections.defaultdict(int)
+total = 0
 
 for bin, count in read_telemetry().items():
     if bin in bin_names:
@@ -37,6 +39,7 @@ for bin, count in read_telemetry().items():
         if len(name) < 3:
             name = " ".join(bin_names[bin].split("_", 2)[0:2])
         CAs[name] += count
+        total += count
 
 with open("CA.csv", "w") as f:
     w = csv.writer(f)
@@ -44,3 +47,24 @@ with open("CA.csv", "w") as f:
     for name, count in sorted(CAs.items(), key=lambda kv: kv[1], reverse=True):
         if count > 0:
             w.writerow([name, count])
+
+def formatter(entry):
+    return f"{entry:,}"
+
+bar_chart = pygal.HorizontalBar(dynamic_print_values=True, pretty_print=True)
+bar_chart.title = "Mozilla Handshakes by CA"
+bar_chart.value_formatter = formatter
+other = 0
+threshold = int(0.0002 * total)
+print("Graphing entries above threshold", threshold)
+
+
+for name, count in sorted(CAs.items(), key=lambda kv: kv[1], reverse=True):
+    # Skip entries below 0.01:
+    if count >  threshold:
+        bar_chart.add(name, count)
+    else:
+        other += count
+
+bar_chart.add("Other", other)
+bar_chart.render_to_file("CA.svg")
